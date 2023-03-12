512e52736dd7669f078e690600098ec5870d7d01
Eknath Kumbhar <54890231+eknath-kumbhar@users.noreply.github.com>
2023-03-12 15:41:02 +0530

    Revert "Task 1 changing qr code style"

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-var QRCodeStyling;
-const urlFieldId = 'url'
+var QRCode;
 
-const urlInput = document.getElementById(urlFieldId);
-const qrCodeContainer = document.getElementById('qr-code');
-let qrCode;
+const urlInput = document.getElementById('url-input');
+const qrCode = document.getElementById("qr-code");
 
 function getCurrentPageUrl() {
-    chrome.tabs && chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
+    chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
         const activeTab = tabs[0] || {};
         activeTab.hasOwnProperty('url') ? setInputValue(tabs[0].url) : '';
     });
@@ -18,40 +16,18 @@ function setInputValue(url) {
 }
 
 function renderQrCode(url) {
-    qrCode = '';
-    qrCode = new QRCodeStyling({
-        width: 232,
-        height: 232,
-        margin: 5,
-        data: url,
-        dotsOptions: {
-            type: 'extra-rounded'
-        },
-        cornersSquareOptions: {
-            type: 'extra-rounded'
-        },
-        cornersDotOptions: {
-            type: 'extra-rounded'
-        },
+    qrCode.innerHTML = '';
+    new QRCode(qrCode, {
+        text: url,
+        width: 200,
+        height: 200,
+        correctLevel: QRCode.CorrectLevel.H
     });
-    qrCodeContainer.innerHTML = '';
-    qrCode.append(qrCodeContainer);
-}
-
-function download() {
-    qrCode.download({ name: 'qr', extension: 'png' });
 }
 
 getCurrentPageUrl();
 
-document.querySelector(`#${urlFieldId}`).addEventListener('change', (e) => {
+document.querySelector('#qr-form').addEventListener('submit', (e) => {
+    e.preventDefault();
     renderQrCode(urlInput.value);
-});
-
-document.querySelector('#close-btn').addEventListener('click', (e) => {
-    window.close();
-});
-
-document.querySelector('#download').addEventListener('click', (e) => {
-    download();
 });
